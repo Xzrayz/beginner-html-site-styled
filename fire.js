24fbe998db6b782ebafba47c54b4e9db751a0d42
Xzrayz <Donovan.Moua24@student.cart.org>
2025-01-24 09:13:26 -0800

    commit

---
+++ fire.js
@@ -0,0 +1,38 @@
+const myImage = document.querySelector("img");
+
+myImage.addEventListener("click", () => {
+  const mySrc = myImage.getAttribute("src");
+  if (mySrc === "images/firefox-icon.png") {
+    myImage.setAttribute("src", "images/firefox2.png");
+  } else {
+    myImage.setAttribute("src", "images/firefox-icon.png");
+  }
+});
+let myButton = document.querySelector("button");
+let myHeading = document.querySelector("h1");
+function setUserName() {
+    const myName = prompt("Donovan");
+    localStorage.setItem("name", myName);
+    myHeading.textContent = `Mozilla is cool, ${myName}`;
+  }
+  if (!localStorage.getItem("name")) {
+    setUserName();
+  } else {
+    const storedName = localStorage.getItem("Donovan");
+    myHeading.textContent = `Mozilla is cool, ${storedName}`;
+  }
+  if (!localStorage.getItem("name")) {
+    setUserName();
+  } else {
+    const storedName = localStorage.getItem("Donovan");
+    myHeading.textContent = `Mozilla is cool, ${storedName}`;
+  }
+  function setUserName() {
+    const myName = prompt("Donovan");
+    if (!myName) {
+      setUserName();
+    } else {
+      localStorage.setItem("name", myName);
+      myHeading.textContent = `Mozilla is cool, ${myName}`;
+    }
+  }
